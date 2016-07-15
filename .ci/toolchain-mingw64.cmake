a7b2c406717d6304f4a2468af7c64ad36f769774
rakiru <rabbitkillrun@googlemail.com>
2016-07-16 00:47:02 +0100

    More changes

--- .ci/toolchain-mingw64.cmake
+++ .ci/toolchain-mingw64.cmake
@@ -5,7 +5,6 @@ set(TOOLCHAIN_PREFIX x86_64-w64-mingw32)
 
 # cross compilers to use for C and C++
 set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
-set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
 set(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
 
 # target environment on the build host system
